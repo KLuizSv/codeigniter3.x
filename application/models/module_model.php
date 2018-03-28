f0d8b5be9b9264d1333d20002462da7bbd3fc75f
KLuizSv <kluizsv@gmail.com>
2018-03-28 16:31:02 -0500

    Modificación de module_model.php

--- application/models/module_model.php
+++ application/models/module_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class module_model extends CI_Model
+class Module_model extends CI_Model
 {
 
 	private $last_id = NULL;
